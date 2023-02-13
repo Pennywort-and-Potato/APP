f264bee6212f3132788dd83fcaf905036174e21a
Nguyễn Bá Khoa <khoa.nguyenba@outlook.com>
2023-02-13 11:16:35 +0700

    remove env config

--- setup.sh
+++ setup.sh
@@ -1,20 +1,4 @@
 #!/bin/sh
-ENV_FILE='.env'
-ENV_CONTENT='
-#GLOBAL CONFIGS\n
-ENVIRONMENT=dev             #dev or prod
-#DATABASE CONFIGS\n
-DBA=postgresql              #Database Adapter\n
-DBH=pg                      #Database Host\n
-DBHP=5432                   #Database Port\n
-DBU=khoa                    #Database User\n
-DBP=Khoa123123              #Database Password\n
-#SERVER_ENV\n
-RAILS_ENV=development\n
-API_PORT=3000\n
-AUTH_SECRET=EMILIAIZDABEZT  #JWT Secret\n
-#CLIENT_ENV\n
-'
 INSTALL GITHUB CLI
 case "$OSTYPE" in
   darwin*)  brew install gh ;;
@@ -35,8 +19,6 @@ gh auth login
 gh repo clone Pennywort-and-Potato/something_server
 gh repo clone Pennywort-and-Potato/something_client
 
-[ -e $ENV_FILE ] && rm "$ENV_FILE"
-echo -e $ENV_CONTENT >> $ENV_FILE
 echo "INSTALLING NEXT DEPENDENCY"
 cd something_client;
 npm i -g yarn || yarn
